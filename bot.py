14d76008cde5b92c9b8437a31f950f07a4db3326
Adishesh <adisheshpantec@yahoo.co.in>
2018-01-03 01:04:12 -0800

    Added commands to alter lo, hi, delay and toggle talk

--- bot.py
+++ bot.py
@@ -5,68 +5,78 @@ import json
 import time
 
 client = discord.Client()
-channel = None
-task = None
+channel, task, delay, lo, hi, talk = None, None, None, None, None, True
 
 @client.event
 async def on_ready():
-    print('Logged in as')
+    print("Logged in as")
     print(client.user.name)
     print(client.user.id)
-    print('------------')
+    print("------------")
 
 @client.event
 async def on_message(message):
-    global task, channel
+    global task, channel, client, delay, lo, hi, talk
     content = message.content
+    args = content.split(" ")
     print(content)
 
-    if content.startswith('!price'):
+    if content.startswith("!price"):
         r = requests.get("https://bitgrail.com/api/v1/BTC-XRB/ticker") 
         j = json.loads(r.text)
-        price = float(j['response']['ask'])
+        price = float(j["response"]["ask"])
         await client.send_message(message.channel, "price is "+str(price))
     
-    elif content.startswith('!monitor'): 
+    elif content.startswith("!delay"): 
+        delay = int(args[1])
+
+    elif content.startswith("!lo"): 
+        lo = float(args[1])
+
+    elif content.startswith("!hi"): 
+        hi = float(args[1])
+
+    elif content.startswith("!talk"): 
+        talk = not talk
+
+    elif content.startswith("!monitor"): 
         if not task: 
             print("Creating task")
-            args = content.split(' ')
             delay, lo, hi = int(args[1]), float(args[2]), float(args[3])
-            if len(args) > 4 and args[4] == 'talk': talk = True
-            else: talk = False
+            talk = len(args) > 4 and args[4] == "talk"
 
-            task = client.loop.create_task(background_loop(delay, lo, hi, talk))
+            task = client.loop.create_task(background_loop())
     
-    elif content.startswith('!stop'): 
+    elif content.startswith("!stop"): 
         if task:
             print("Stopping task")
             await client.send_message(channel, "Stopping monitor")
             task.cancel()
             task = None
 
-    elif message.content.startswith('!sleep'):
+    elif message.content.startswith("!sleep"):
         await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
+        await client.send_message(message.channel, "Done sleeping")
 
-async def background_loop(delay, lo, hi, talk):
-    global channel, client
+async def background_loop():
+    global channel, client, delay, lo, hi, talk
     peak = None
     peakTime = None
 
     while not channel: 
         await client.wait_until_ready()
-        channel = client.get_channel("****************")
+        channel = client.get_channel("CHANNEL_ID")
         await asyncio.sleep(20)
 
     await client.send_message(channel, "Starting monitor")
 
     while not client.is_closed:
 
-        print('Querying', end=' ')
-        r = requests.get('https://bitgrail.com/api/v1/BTC-XRB/ticker') 
+        print("Querying", end=" ")
+        r = requests.get("https://bitgrail.com/api/v1/BTC-XRB/ticker") 
         j = json.loads(r.text) 
-        price = float(j['response']['ask']) 
-        print('price={}'.format(price), end=' ')
+        price = float(j["response"]["ask"]) 
+        print("price={}".format(price), end=" ")
 
         if not peak: peak = price
         if not peakTime: peakTime = time.time()
@@ -80,7 +90,7 @@ async def background_loop(delay, lo, hi, talk):
         elif delta < 0:
             peak = price
             peakTime = time.time()
-        print('peak={}'.format(peak))
+        print("peak={}".format(peak))
             
         if price < lo: 
             await client.send_message(channel, "price {0:.8f} has DROPPED below lower threshold".format(price), tts = talk)
@@ -89,4 +99,4 @@ async def background_loop(delay, lo, hi, talk):
 
         await asyncio.sleep(delay)
         
-client.run('**************************')
+client.run("BOT_KEY")
